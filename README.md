fe52d556ea836e93d5fca02211f74aefd0f73692
Julio Moura <juliomourajr@hotmail.com>
2024-12-02 17:13:34 -0300

    :tada: Commit inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+### Projeto Final ADA tech Serviços Cloud
+
+## Cenário
+
+A Ada Contabilidade enfrenta um desafio operacional diário: os contadores precisam enviar arquivos manualmente para armazenamento e, em seguida, registrar no banco de dados a quantidade de linhas contidas nesses arquivos. Esse processo manual é ineficiente e propenso a erros.
+
+Será Criada uma solução para automatizar a arquitetura em todo o seu fluxo, se baseando em práticas DevOps para simplificar o fluxo de trabalho e garantir a confiabilidade do processo.
+
+O código vai enviar arquivos de forma automática para um bucket s3 de forma automatizada.
+
+Usar os serviços da AWS, como SNS, SQS, Lambda e ElasticCache, Gravando em um banco de dados o nome do arquivo e o número de linhas.
---
+++ criarcsv.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime
+
+
+transacoes = [
+    {'data': '01/01/2023', 'descricao': 'Venda de produto A', 'valor': 30000},
+    {'data': '02/01/2023', 'descricao': 'Compra de material B', 'valor': -500},
+    {'data': '03/01/2023', 'descricao': 'Serviço prestado C', 'valor': 2000},
+    {'data': '04/01/2023', 'descricao': 'Despesa com transporte', 'valor': -300},
+    {'data': '05/01/2023', 'descricao': 'Venda de produto D', 'valor': 15000},
+    {'data': '06/01/2023', 'descricao': 'Compra de material E', 'valor': -700},
+    {'data': '07/01/2023', 'descricao': 'Serviço prestado F', 'valor': 2500},
+    {'data': '08/01/2023', 'descricao': 'Despesa com marketing', 'valor': -1200},
+    {'data': '09/01/2023', 'descricao': 'Venda de produto G', 'valor': 18000},
+    {'data': '10/01/2023', 'descricao': 'Compra de material H', 'valor': -800},
+]
+
+agora = datetime.now()
+nome_arquivo = agora.strftime("%d-%m-%Y") + "-Relatorio-Contabil.csv"
+
+valor_total = sum(transacao['valor'] for transacao in transacoes)
+
+with open(nome_arquivo, mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['data', 'descricao', 'valor'])
+    writer.writeheader()
+    for transacao in transacoes:
+        writer.writerow(transacao)
+    writer.writerow({'data': '', 'descricao': 'Total', 'valor': valor_total})
+
+
+with open(nome_arquivo, mode='r') as file:
+    reader = csv.reader(file)
+    linha_count = sum(1 for row in reader)
+
+print(f"O arquivo {nome_arquivo} contém {linha_count - 1} transações financeiras.") 
